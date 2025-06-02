edb959852101741a627b4eb33a5ef7d007d9d53a
abhi7277 <129675515+abhi7277@users.noreply.github.com>
2025-06-03 01:34:50 +0530

    Add files via upload
    1st assignment

---
+++ task 1.py
@@ -0,0 +1,14 @@
+#calculation
+a=input("enter first number")
+b=input("enter second number")
+a=int(a)
+b=int(b)
+
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print('addition: ',add)
+print('subtraction: ',sub)
+print('multiplication: ',multi)
+print('division: ',div)
---
+++ task 2 .py
@@ -0,0 +1,5 @@
+#greeting
+a=input('ENTER YOUR FIRST NAME')
+b=input('ENTER YOUR SECOND NAME')
+
+print("hello,"+a+" " +b+"!" " welcome to the python program")
